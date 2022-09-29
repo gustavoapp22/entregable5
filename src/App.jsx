14784f7a5b68588c6ea45e5ee5701b9e5f1a8f8b
gustavo <gustavo>
2022-09-29 17:43:27 -0500

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import './App.css'
 import { HashRouter, Route, Routes } from "react-router-dom";
 import Users from './components/Users'
 import Pookedexx from './components/Pookedexx';
-
-//  import ProtectedRoutes from './components/ProtectedRoutes';
 import Red from './components/Red';
 import Pokedex from './components/pokedex';
 
@@ -17,13 +15,11 @@ function App() {
       <div className='App'>
         <Routes>
           <Route path="/" element={<Users />} />
-           {/* <Route element={<ProtectedRoutes/>}> */}
 
           <Route path="/pokedex/:name" element={<Pokedex />} />
           <Route path="/pokedexx/:id" element={<Pookedexx />} />
           <Route path="/red" element={<Red/>} />
           
-           {/* </Route> */}
         
 
         </Routes>
